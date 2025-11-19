03ef667b0251f5bd3b1b012b525684e0146a4e79
Manan Ahmed Broti <54348038+AhmedManan@users.noreply.github.com>
2025-11-20 02:32:56 +0600

    Initial Movie Database API
    * Added main.py

--- README.md
+++
@@ -1,4 +0,0 @@
-# Blog_API_Project
-Here is an implimentation for a Movie Database & Review API in **FastAPI** in **Python**. In this blog API project I have implemented all types of http methods 'Get', 'Post', 'Put', 'Delete'. 
-
-# Table of Content
--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-from fastapi import FastAPI, status
-import models
-from database import engine
-from files.routers import blog, user, authentication
+from fastapi import FastAPI
 
+app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"message": "Movie Database API. Powered by MananAcademy.com"}
 
-app = FastAPI(
-    openapi_prefix="",
-    title="Blog API",
-    version="1.0.0",
-    description="This API is Created by the developer Manan Ahmed Broti. Not for commercial purpose. The Base URL for this API is set to: https://blog-rhhb.onrender.com/",
-)
-models.Base.metadata.create_all(bind=engine)
-app.include_router(authentication.router)
-app.include_router(blog.router)
-app.include_router(user.router)
-
-
-@app.get("/", status_code=status.HTTP_200_OK)
-async def credits():
-    return {"message": "Welcome to the Blog API. Developed By Manan Ahmed Broti. Website: www.AhmedManan.com"}
-
+# 127.0.0.1/movies/1?query=api
+@app.get("/movies/{movie_id}")
+async def get_movie(movie_id: int, query: str = None):
+    return {"movie_id": movie_id, "query": query}
